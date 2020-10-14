22c7f60f339673b8dbc956d0176fa1b07175ffbb
EvilBlackBerry <72876483+EvilBlackBerry@users.noreply.github.com>
2020-10-14 20:32:57 +0500

    Add files via upload

---
+++ main (1).swift
@@ -0,0 +1,10 @@
+let a = 8.0
+let b = 6.0
+let d = a * a + b * b
+let c = d.squareRoot()
+let p = a + b + c
+let s = a * b / 2
+
+print ("Гипотенуза = \(c)")
+print ("Периметр = \(p)")
+print ("Площадь = \(s)")
